841f3613000c3841203683fbfef72655d92ce245
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-02 09:23:15 -0700

    Order Views and Home views  are built

---
+++ Pierre/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+
+namespace Pierre.Controllers
+{
+  public class OrderController : Controller
+  {
+
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(string title)
+    {
+      Order myOrder = new Order(title);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/orders/{id}")]
+    public ActionResult Show(int id)
+    {
+      Order foundOrder = Order.Find(id);
+      return View(foundOrder);
+    }
+  }
+}
